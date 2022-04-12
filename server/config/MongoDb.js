4f9f02a0a12984a0a4befab23ff6fe8cb83166ca
oluwadamoo <mrdamilolasaliu@gmail.com>
2022-04-12 11:29:11 +0100

    Make it better

--- server/config/MongoDb.js
+++ server/config/MongoDb.js
@@ -1,23 +1,17 @@
 import mongoose from "mongoose";
-
 const connectDatabase = async () => {
-  const MONGO_URL =
-    process.env.MONGO_URL ||
-    "mongodb+srv://saliu-damilola:2905omolara@cluster0.imown.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-  // console.log(MONGO_URL, "mongo");
+  console.log(process.env.MONGO_URL, "env...");
 
+  const MONGO_URL = process.env.MONGO_URL;
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://saliu-damilola:2905omolara@cluster0.imown.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    const conn = await mongoose.connect(MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
     console.log(`MongoDB Connected`);
   } catch (error) {
-    // console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import userRouter from "./Routes/UserRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 
 dotenv.config();
+
 connectDatabase();
 const app = express();
 app.use(express.json());
@@ -27,6 +28,6 @@ app.get("/api/config/paypal", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`server run in port ${5000}`));
+app.listen(PORT, console.log(`server run in port ${5000}`));
